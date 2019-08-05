4ae7f09b9fc1589dd096b09315f271b7092e94bc
laiharry <lai.harry@gmail.com>
2019-08-05 14:28:31 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 var express = require('express'),
     fs = require('fs'),
-    app = express();
-//    eps = require('ejs'),
+    app = express(),
+    eps = require('ejs');
 //    morgan = require('morgan');
+
+// const http = require('http').Server(app);
+
 
 var app = express();
 
 var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var PORT = process.env.PORT || 3000;
 
 // app is running!
 app.get('/', function(req, res) {
-    res.send('Hello from NodeJS  at '+ new Date());
+    res.render('index.ejs');
+//    res.send('Hello from NodeJS  at '+ new Date());
 });
 
 app.listen(8080, ip);
